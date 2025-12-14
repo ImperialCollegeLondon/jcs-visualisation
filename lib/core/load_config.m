f3f4e25c2112dc6eae2fa30ccd7281749ce5265c
JJ <jonathanjamesholt@gmail.com>
2025-12-14 15:05:35 +0000

    wip left handed matrices

--- lib/core/load_config.m
+++ lib/core/load_config.m
@@ -45,6 +45,25 @@ function [is_right_knee, state, setup, transforms] = load_config(path)
     transforms.from_digitiser.gTt0 = defineBodyFixedFrameTibia(tibia, is_right_knee);
 
 
+    figure
+    visualise_landmark(tibia, femur, is_right_knee, '', '-', [0 0 0]);
+    xlabel("x"); ylabel("y"); zlabel("z");
+
+    figure
+    visualise_matrix(transforms.from_digitiser.gTf0, '-');
+    hold on;
+    visualise_matrix(transforms.S1_T_RB1, '-');
+    hold off;
+
+    legend(["From digitisation", "From simvitro"]);
+
+    figure;
+
+    visualise_matrix(state.JCS.T_World1_World2_for_Fiducials);
+    visualise_matrix(state.JCS.T_World1_World2);
+    visualise_matrix(mat_to_left_handed(state.JCS.T_World1_World2));
+    legend(["For fiducials", "Simvitro left knee", "flipped"]);
+
 
     transforms.gTee = setup.DefineRobotCoordinateSystem.T_WORLD1_ROB;
 end
