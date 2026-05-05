1fbba3b7027fec30912cd2fc65b6ffff56ee293a
JJ <jonathanjamesholt@gmail.com>
2026-05-05 11:29:47 +0100

    add: Digitisation class to simplify the recreation of kinematics

---
+++ lib/@Digitisation/Digitisation.m
@@ -0,0 +1,17 @@
+classdef Digitisation
+    properties
+        Tibia
+        Femur
+    end
+    methods (Access = private)
+        function Digitisation()
+        end
+    end
+
+    methods
+        function tibia()
+        end
+        function femur()
+        end
+    end
+end
